b55de10a8b002f7ca7de655036f74ab838bac541
FiliLecci <61226793+FiliLecci@users.noreply.github.com>
2022-04-14 23:09:25 +0200

    Add files via upload

---
+++ index.mjs
@@ -0,0 +1,25 @@
+import fetch from "node-fetch";
+import fs from "fs";
+import https from "https";
+
+let key = "kXyDzzUrJ8QEkAxNSEhjnYgbmIrQnymB5Vn3HgDa";
+let url = ["https://api.nasa.gov/planetary/apod?api_key=", key].join("");
+console.log(url);
+
+let response = await fetch(url);
+let json = await response.json();
+let imageUrl = json['url'];
+
+console.log(json);
+
+let imagePath = "C:/Users/user/Desktop/immagini/" + json["title"] + "-" + json["copyright"] + ".jpg";
+
+const file = fs.createWriteStream(imagePath);
+const request = https.get(imageUrl, function(response) {
+   response.pipe(file);
+   // after download completed close filestream
+   file.on("finish", () => {
+       file.close();
+       console.log("Download Completed");
+   });
+});
