983635c7c2d3f9aa1437a9b800726fea0f6e1c4d
FiliLecci <61226793+FiliLecci@users.noreply.github.com>
2022-04-14 23:45:17 +0200

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -6,14 +6,21 @@ let key = "DEMO_KEY";
 let url = ["https://api.nasa.gov/planetary/apod?api_key=", key].join("");
 console.log(url);
 
+//richiesta al server REST
 let response = await fetch(url);
+//conversione della risposta in codice JSON
 let json = await response.json();
-let imageUrl = json['url'];
+//parse del codice JSON per ottenere l'URL dell'immagine
+let imageUrl = json['url']; //json["hdurl"] per immagini in HD (non sempre sono di qualità superiore)
 
+//stampa del codice JSON perché può fare comodo e fa figo
 console.log(json);
 
-let imagePath = "C:/Users/user/Desktop/immagini/" + json["title"] + "-" + json["copyright"] + ".jpg";
+//imagePath è la directory in cui verrà salvata l'immagine ed il suo nome
+let imagePath = "C:/Users/user/Desktop/immagini/" + json["title"] + "-" + json["copyright"] + ".jpg"; //nota: tenere come estensione .jpg in quanto tutte le immagini sono jpg
 
+//codice preso da: https://stackoverflow.com/questions/11944932/how-to-download-a-file-with-node-js-without-using-third-party-libraries
+//(grazie stackoverflow che mi salvi il culo)
 const file = fs.createWriteStream(imagePath);
 const request = https.get(imageUrl, function(response) {
    response.pipe(file);
